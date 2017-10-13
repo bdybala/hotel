d5232480f99152798f09f0a317e474a1e9323454
Bartosz Dybała <dybala.bartosz@gmail.com>
2017-10-13 21:41:30 +0200

    add routing

--- src/main/webapp/js/config/routing.js
+++ src/main/webapp/js/config/routing.js
@@ -1,12 +1,19 @@
 app.config(['$routeProvider',function($routeProvider) {
+
 	$routeProvider
 	.when("/", {
-		templateUrl : "views/mainPage.html"
+		templateUrl : "views/mainPage.html",
+		controller : 'MainPageCtrl'
 	})
 	.when("/login", {
-		templateUrl : "views/loginPage.html"
+		templateUrl : "views/loginPage.html",
+		controller : 'LoginPageCtrl'
+	})
+	.when("/client", {
+		templateUrl : "views/client/clientPage.html"
 	})
 	.otherwise({
 		redirectTo : "/"
 	});
+
 }]);
