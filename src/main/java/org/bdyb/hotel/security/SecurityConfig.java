294067f83dd32905227f29cb92e46643424ff349
Bartek Dybała <dybalix@gmail.com>
2017-11-15 21:32:09 +0100

    change security

--- src/main/java/org/bdyb/hotel/security/SecurityConfig.java
+++ src/main/java/org/bdyb/hotel/security/SecurityConfig.java
@@ -4,12 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //TODO
+    private static final String[] RESOURCES = new String[]{
+            "/web"
+    };
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -19,12 +25,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("*").permitAll()
                 .and()
-            .logout()
-                .permitAll();
+
+                .formLogin()
+//                .loginPage("/login.html")
+                .and()
+                .logout()
+//                .logoutUrl("?logout")
+//                .deleteCookies("JSESSIONID")
+                .and().csrf().disable();
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(RESOURCES);
+    }
 }
--- src/main/java/org/bdyb/hotel/security/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package org.bdyb.hotel.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
