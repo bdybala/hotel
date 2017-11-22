7a2c6efd83557525aec744ea268617ea6c9d7e35
Bartek Dybała <dybalix@gmail.com>
2017-11-22 01:14:03 +0100

    fix cross domain requests and change api (add 's' to the url)

--- src/main/java/org/bdyb/hotel/SpringConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.bdyb.hotel;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class SpringConfiguration {
-}
--- src/main/java/org/bdyb/hotel/controller/PriceController.java
+++ src/main/java/org/bdyb/hotel/controller/PriceController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/price")
+@RequestMapping("/api/prices")
 public class PriceController {
 
     @Autowired
